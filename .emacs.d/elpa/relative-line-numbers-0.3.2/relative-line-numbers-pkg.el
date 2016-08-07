e12b0b00cf8133a43f129492eacf4bf8be3cfe0c
Mahdi Abdinejadi <mehdi61b@gmail.com>
2016-08-07 09:43:51 +0200

    Added my .emacs.d folder plus minor alias change.

---
+++ .emacs.d/elpa/relative-line-numbers-0.3.2/relative-line-numbers-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "relative-line-numbers" "0.3.2" "Display relative line numbers on the margin" '((emacs "24")) :authors '(("Fanael Linithien" . "fanael4@gmail.com")) :maintainer '("Fanael Linithien" . "fanael4@gmail.com") :url "https://github.com/Fanael/relative-line-numbers")
